248f28d059bf852fc55361f7e04d299f134d9363
EsraOzdogan <50195220+EsraOzdogan@users.noreply.github.com>
2020-02-08 18:43:28 +0300

    sql files

---
+++ SporTakımMaçOyuncuArşivi/SporTakımMaçOyuncuArşivi/delete.sql
@@ -0,0 +1,18 @@
+Use SporTakýmMaçOyuncuArþivi;
+--FutbolcuTakýmSözleþme tablosundan bastarihi 200-10-19 olan satýrý siler 
+
+DELETE FROM tblFutbolcuTakýmSözleþme 
+WHERE Bas_tar= '2000-10-19'
+--TRUNCATE TABLE tblFutbolcuTakýmSözleþme
+--FutbolcuTakýmSözleþme tablosundan baþlangýç tarigi 1998-01-05 olan satýrý siler
+DELETE FROM tblFutbolcuTakýmSözleþme WHERE Bas_tar='1998-01-05'
+
+--TakýmSezon tablosundan attýpý gol sayýsý 5 ten büyük olanlarý siler 
+DELETE FROM tblTakýmSezon WHERE AttýðýGol_Say>5
+
+
+--TeknikDirektörTakýmSözleþme tablosunda maaþý 1500000.0000den büyük olanlarý sil
+DELETE FROM tblTeknikDirektörTakýmSözleþme WHERE Maaþ>'1500000.0000'
+
+--Stadyum tablosundaki stadyum adý Craven Cottage olan satýrý sil
+DELETE FROM tblStadyum WHERE Stadyum_Ad='Craven Cottage'
---
+++ SporTakımMaçOyuncuArşivi/SporTakımMaçOyuncuArşivi/update.sql
@@ -0,0 +1,20 @@
+USE SporTakýmMaçOyuncuArþivi;
+--Sezon tablosundan Yýl2='1924-12-30 ' olan satýrýn sezon adýný Super loto super lig ve yýl1='2018-09-04' 
+--olarak güncelle
+UPDATE tblSezon SET Sezon_Ad='Super Loto Super Lig', Yýl1='2018-09-04' WHERE Yýl2='1924-12-30 '
+
+--FutbolcuTAkýmSözleþme tablosundan Bit_TAr = NULL olan satýrýn tarihi Bit_Tar = '2011-05-09'
+--olarak güncelle 
+UPDATE tblFutbolcuTakýmSözleþme SET Bas_Tar='2011-06-09' WHERE Bit_Tar = '2005-12-08'
+
+USE SporTakýmMaçOyuncuArþivi;
+--Futbolcu tablosundan Futbolcu_Ad='Ander Herrera' ve PozisyonID si ='3' olan satýrý PozisyonID='4'
+--olarak güncelle
+
+UPDATE tblFutbolcu SET Forma_Num='10' WHERE Futbolcu_Ad='Ander Herrera' AND Forma_Num='21'
+
+--Maç tablosundaki  Saat=17.00 olan ve Tarih=1923-03-10 olan satýrýn Saat=20.00 olarak güncelle
+UPDATE tblMaç SET Saat='20:00' Where Saat='17:00' AND Tarih='1923-03-10'
+--TeknikDirektör tablosundaki TekDir_Ad='Julen Lopetegui' olan satýrý 
+--TekDir_Ad='Zinedine Zidane' olarak güncelle 
+UPDATE tblTeknikDirektör SET TekDir_Ad='Zinedine Zidane' WHERE TekDir_Ad='Julen Lopetegui'
